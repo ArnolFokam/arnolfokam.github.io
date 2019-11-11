69d163f5d981713467918aa48f9dc95187067aba
arnolfokam <arnolfokam23@gmail.com>
2019-11-11 11:57:52 +0100

    rebuilding site Mon Nov 11 11:57:51 WAT 2019

--- js/main.js
+++ js/main.js
@@ -19,6 +19,15 @@ jQuery(document).ready(function() {
     jarallax();
     data_images();
     anchor();
+    totop();
+    totop_myhide();
+    contact_form();
+
+    jQuery(window).on('scroll',function(){
+        //e.preventDefault();
+        totop_myhide();
+        
+    });
 
     jQuery(window).on('resize', function() {
         miniboxes();
@@ -224,4 +233,75 @@ function data_images(){
 		var url				= element.data('img-url');
 		element.css({backgroundImage: 'url('+url+')'});
 	});
+}
+
+function totop(){
+    
+    "use strict";
+    
+    jQuery(".totop").on('click', function(e) {
+        e.preventDefault();     
+        jQuery("html, body").animate({ scrollTop: 0 }, 'slow');
+        return false;
+    });
+}
+
+function totop_myhide(){
+    
+    "use strict";
+    
+    var toTop       =jQuery(".totop");
+    if(toTop.length){
+        var topOffSet   =toTop.offset().top;
+        
+        if(topOffSet > 1000){
+            toTop.addClass('opened');   
+        }else{
+            toTop.removeClass('opened');
+        }
+    }
+}
+
+
+
+function contact_form(){
+    
+    "use strict";
+    
+    jQuery(".contact_form #send_message").on('click', function(){
+        
+        var name        = jQuery(".contact_form #name").val();
+        var email       = jQuery(".contact_form #email").val();
+        var message     = jQuery(".contact_form #message").val();
+        var subject     = jQuery(".contact_form #subject").val();
+        var success     = jQuery(".contact_form .returnmessage").data('success');
+    
+        jQuery(".contact_form .returnmessage").empty(); //To empty previous error/success message.
+        //checking for blank fields 
+        if(name===''||email===''||message===''){
+            
+            jQuery('div.empty_notice').slideDown(500).delay(2000).slideUp(500);
+        }
+        else{
+            // Returns successful data submission message when the entered information is stored in database.
+            jQuery.post("https://formspree.io/mvookdob",{ ajax_name: name, ajax_email: email, ajax_message:message, ajax_subject: subject}, function(data) {
+                
+                jQuery(".contact_form .returnmessage").append(data);//Append returned message to message paragraph
+                
+                
+                if(jQuery(".contact_form .returnmessage span.contact_error").length){
+                    jQuery(".contact_form .returnmessage").slideDown(500).delay(2000).slideUp(500);     
+                }else{
+                    jQuery(".contact_form .returnmessage").append("<span class='contact_success'>"+ success +"</span>");
+                    jQuery(".contact_form .returnmessage").slideDown(500).delay(4000).slideUp(500);
+                }
+                
+                if(data===""){
+                    jQuery("#contact_form")[0].reset();//To reset form fields on success
+                }
+                
+            });
+        }
+        return false; 
+    });
 }
